84e294442a54ad6bd001916d6887113b94a6a25d
Scott <scottunogundersen@gmail.com>
2024-04-19 12:16:07 +0200

    Done

---
+++ guide/src/App.js
@@ -0,0 +1,16 @@
+import './index.css';
+
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Home />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
